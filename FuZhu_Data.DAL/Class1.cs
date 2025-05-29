707e857df97665c784ebf38a1ad93403bc9ccd9a
49838657@qq.com <49838657@qq.com>
2025-05-29 17:41:44 +0800

    修改说明

--- FuZhu_Data.DAL/Class1.cs
+++ FuZhu_Data.DAL/Class1.cs
@@ -8,6 +8,14 @@ namespace FuZhu_Data.DAL
 {
     public class Class1
     {
+        public static string GetData(string sql)
+        { 
+            return "返回一个值";
+        }
+        public static string GetData(string sql, string[] parms)
+        {
+            return "返回一个值22";
+        }
 
     }
 }
