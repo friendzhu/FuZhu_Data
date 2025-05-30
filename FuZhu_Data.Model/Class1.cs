9309b8f38ce555f461399650e9a3be619ad01581
49838657@qq.com <49838657@qq.com>
2025-05-30 10:48:52 +0800

    aaa

--- FuZhu_Data.Model/Class1.cs
+++ FuZhu_Data.Model/Class1.cs
@@ -2,6 +2,9 @@
 {
     public class Class1
     {
-
+        public string Name { get; set; } = "FuZhu_Data.Model";
+        public string Version { get; set; } = "1.0.0";
+        public string Description { get; set; } = "This is a model class for the FuZhu_Data project.";
+        public string Author { get; set; } = "Your Name";
     }
 }
